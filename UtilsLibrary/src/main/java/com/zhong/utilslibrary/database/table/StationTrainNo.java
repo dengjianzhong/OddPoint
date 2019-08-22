ee47745f09537ed800f4056ed9c188b954dcccae
dengjianzhong <330035231@qq.com>
2019-08-22 10:17:59 +0800

    更新

---
+++ UtilsLibrary/src/main/java/com/zhong/utilslibrary/database/table/StationTrainNo.java
@@ -0,0 +1,40 @@
+package com.zhong.utilslibrary.database.table;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.io.Serializable;
+
+@DatabaseTable(tableName = "StationTrainNo")
+public class StationTrainNo implements Serializable {
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(columnName = "station_train_code")
+    private String station_train_code;
+    @DatabaseField(columnName = "train_no")
+    private String train_no;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStation_train_code() {
+        return station_train_code;
+    }
+
+    public void setStation_train_code(String station_train_code) {
+        this.station_train_code = station_train_code;
+    }
+
+    public String getTrain_no() {
+        return train_no;
+    }
+
+    public void setTrain_no(String train_no) {
+        this.train_no = train_no;
+    }
+}
